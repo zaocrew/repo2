d3f576a89ab22299810e8219d2da65c912f0c585
Your Name <you@example.com>
2023-01-19 18:04:26 +0300

    Первый коммит

---
+++ main.js
@@ -0,0 +1,64 @@
+// console.log("Hello world") // строка
+// console.log(666) // число
+// console.log(66+66)
+// console.log('Hello ' + ' world')
+// alert('Hello world')
+// const myBirthday = '24.06.1996' неизменяемое переменная
+// myBirthday = '01.01.1901'
+// let myBirthday = '12.12.12'
+// myBirthday = '1/1/1'
+// console.log(myBirthday)
+
+// console.log('06')
+// console.log("'Добро '+'пожаловать '+' на курс' '+' 'Никита'")
+// alert("17.01.2023")
+
+document.getElementById('first').innerHTML -'Hello World'
+
+// 1 тип данных числовой (number)
+// console.log(Infinity); //infinity
+// console.log(1/0); //infinity
+// console.log(NaN); // получаем NAN
+// console.log('storlka' * 12); //получаем NAN
+
+// 2 тип данных BigInt
+// let a = 999999999999
+// console.log(a)
+
+// 3 тип данных строка (string)
+// let str1 ="1111";
+// let srt2 ="2222 $(str1)";
+// let str3 = '3333 $(str1)';
+
+// console.log(str1)
+// console.log(str2)
+// console.log(str3)
+
+// 4 тип данных Логический тип данных (BOOLean)
+
+// Let Datcheck -true
+// Let DatunCheck -false
+// // Let a - 4 >1
+// console.log(true == 0)
+
+// 5 тип данных null
+// Let nuller - null
+// console.log(nuller)
+
+// 6 тип данных undifined
+
+// Let test;
+// console.log(test)
+
+// 7 тип данных Обьекты
+
+// 8 оператор typeof
+
+// console.log(typeof undefined)
+// console.log(typeof 0)
+// console.log(typeof 1100n)
+// console.log(typeof false)
+// console.log(typeof Symbol ('id'))
+// console.log(typeof null)
+
+
